ba0fd86b73daf5eed842d8f94fa7dd2b3055eb7a
Artem <artemletmein@gmail.com>
2019-01-24 11:30:39 +0200

    minor changes: add if statement

--- sortArrayByProvidedLetter.js
+++ sortArrayByProvidedLetter.js
@@ -1,12 +1,16 @@
-let arr1 = ['One','Two','Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+let arr1 = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
 
 function sortByLetter(arr, n) {
     let l = new RegExp(n, 'gi');
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         let y = arr[i].split('');
-        y.forEach(function(item){
-            let count = item.match(l);
-            });
+        y.forEach(function(item) {
+            if (item.match(l)) {
+                console.log(y.join(''));
+            } else {
+                console.log(item.match(l))
+            }
+        });
     }
 };
 sortByLetter(arr1, 'e');

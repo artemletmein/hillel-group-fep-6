0d8ef22931a6a32e6b1dcdd2509547d3ad4045b8
Artem <artemletmein@gmail.com>
2019-01-24 11:08:27 +0200

    optimize code

--- sortStringsByLengthInsideArray.js
+++ sortStringsByLengthInsideArray.js
@@ -1,19 +1,17 @@
 /////////////////////////////////////////////////////////////////////////////
 let str = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
-/////////////////////////////////////////////////////////////////////////////
+/////VARIANT #1//////////////////////////////////////////////////////////////
 function buildAstring1(arr, n) {
   let x = [];
     arr.forEach(function(i) {
-        console.log(i.length <= n);
         if (i.length > n) {
           x.push(i);
         }
     });
-    console.log(x);
     return str = x;
 }
 buildAstring1(str, 3);
-////////////////////////////////////////////////////////////////////////////
+/////VARIANT #1////////////////////////////////////////////////////////////
 function buildAstring2(arr, n) {
   let x = [];
     for(let i = 0; i < arr.length; i++) {
@@ -21,7 +19,6 @@ function buildAstring2(arr, n) {
           x.push(arr[i])
         }
     };
-    console.log(x)
     return x;
 };
 buildAstring2(str, 3);
